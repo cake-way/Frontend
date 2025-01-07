2a3e2285468918b05e75bd7a1af181cf16f94e1b
지민재 <jim400934@gmail.com>
2025-01-07 15:44:38 +0900

    Feat: Header 컴포넌트 구현

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,23 @@
+import Header from './components/Header';
+import CakeWay from '../../public/cake-way.svg';
+import Alarm from '../../public/alarm.svg';
+import Image from 'next/image';
+import InputField from './components/InputField';
+
 export default function Home() {
-  return <div>asdf</div>;
+  return (
+    <div>
+      <Header
+        leftButtonImage={
+          <Image src={CakeWay} alt="Cake Way" width={24} height={24} />
+        }
+        centerComponent={
+          <InputField placeholder=" 원하는 케이크를 찾으러 가기" />
+        }
+        rightButtonImage={
+          <Image src={Alarm} alt="Alarm" width={24} height={24} />
+        }
+      />
+    </div>
+  );
 }
