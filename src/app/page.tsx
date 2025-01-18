c308c795b726b85368748b0536e8c6515f4eb7df
류원 <2145007@ewha.ac.kr>
2025-01-18 14:04:57 +0900

    feat : 랜딩페이지 애니메이션 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,10 @@
 'use client';
 
 import Image from 'next/image';
-
 import CakeWayLogo from '../../public/login-images/cake-way.svg';
 import { useRouter } from 'next/navigation';
 import { useEffect } from 'react';
-import { motion } from 'motion/react';
+import { motion } from 'framer-motion';
 
 const LandingPage = () => {
   const router = useRouter();
@@ -17,18 +16,44 @@ const LandingPage = () => {
     return () => {
       clearTimeout(timer);
     };
-  });
+  }, []);
 
   return (
     <main className="w-full h-screen bg-black flex flex-col items-center text-white font-sans">
-      <motion.section className="mt-[300px]">
+      <motion.section
+        className="mt-[300px]"
+        initial={{ rotateX: -90 }}
+        animate={{ rotateX: 0 }}
+        transition={{ duration: 0.8, ease: 'easeOut' }}
+      >
         <Image src={CakeWayLogo} alt="cake-way 로고" />
       </motion.section>
 
-      <motion.section className="mb-[275px] text-center text-md">
-        <span className="mt-[17px]">쉽고 빠른</span>
-        <span className="mt-[17px]">케이크 주문은</span>
-        <span className="mt-[17px]">케이크 웨이로</span>
+      <motion.section className="mb-[275px] text-center text-md flex ">
+        <motion.p
+          className="mt-[17px]"
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5, delay: 0.8 }}
+        >
+          쉽고 빠른
+        </motion.p>
+        <motion.p
+          className="mt-[17px]"
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5, delay: 1.0 }}
+        >
+          &nbsp;케이크 주문은
+        </motion.p>
+        <motion.p
+          className="mt-[17px]"
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5, delay: 1.2 }}
+        >
+          &nbsp;케이크 웨이로
+        </motion.p>
       </motion.section>
     </main>
   );
